6eb2a8670e49a2712a03c7db925b1df1f1e8c084
fwsmit <fw.smit01@gmail.com>
2020-12-23 17:22:37 +0100

    Set maximum record time

--- camera_example.py
+++ camera_example.py
@@ -10,14 +10,23 @@ print("Using openCV version ", cv2.__version__)
 #  camera = Camera.fromEnum(CameraMode.RAW_480_30)
 camera = Camera(960,540,15,VideoFormats.mjpeg)
 
+frame_count = 0
+max_time = 5
+max_frames = 15 * max_time
+
 while(True):
     ret = camera.captureFrame()
-    if ret is True:
+    if ret is True and frame_count < max_frames:
         # Waits for a user input to quit the application
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         # the camera failed to record a frame so abort
+        # or the max number of frames is exceeded
+        if frame_count >= max_frames:
+            print("Maximum recording time exceeded")
         break
+
+    frame_count +=1
 
 camera.release()
