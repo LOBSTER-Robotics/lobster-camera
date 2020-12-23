11b7d06a9ab3212723e1bf0c1429d36aecd99a4d
fwsmit <fw.smit01@gmail.com>
2020-12-23 16:33:36 +0100

    Improve tests

--- simple-opencv-test-headless.py
+++ simple-opencv-test-headless.py
@@ -1,17 +1,15 @@
-# import the opencv library 
-import cv2 
-  
-  
-# define a video capture object 
-vid = cv2.VideoCapture(0) 
-  
-while(True): 
-      
-    # Capture the video frame 
-    # by frame 
-    ret, frame = vid.read() 
-  
-# After the loop release the cap object 
-vid.release() 
-# Destroy all the windows 
-cv2.destroyAllWindows() 
+# import the opencv library
+import cv2
+
+camera_number = 0
+
+# define a video capture object
+vid = cv2.VideoCapture(camera_number)
+
+if vid.isOpened():
+    print("Opening succeeded")
+else:
+    print("Could not open camera {}".format(camera_number))
+
+# After the loop release the cap object
+vid.release()
